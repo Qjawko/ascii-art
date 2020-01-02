d4835d53621979ba7fec74af86c7c8a8b188ad56
Qjawko <Qjawko@gmail.com>
2020-01-02 16:38:08 +0600

    added test cases

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 import subprocess
 
-cases = ['"hello"', '"HELLO"', '"HeLlo HuMaN"', ]
-subprocess.check_output(['./ascii-art', 'hello'])
+cases = ['hello', 'HELLO', 'HeLlo HuMaN', '1Hello 2There',
+         'Hello\\nThere', '{Hello & There #}', 'hello There 1 to 2!',
+         'MaD3IrA&LiSboN']
+
+for case in cases:
+    result = subprocess.check_output(['./ascii-art', case], stderr = subprocess.STDOUT, text = True)
+    print(result)
